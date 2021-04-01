4c7683042264ccf2b029305f3f4634083d0edac0
anusu90 <anusu90@gmail.com>
2021-04-02 00:48:26 +0530

    socket signal

---
+++ public/script.js
@@ -0,0 +1,87 @@
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement("video")
+myVideo.muted = true;
+
+
+var socket = io(); // connection to socket IO server has been made here.
+let myVideoStream, myDisplayStream;
+
+const peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "3030"
+});
+
+connectToNewUser = (userID, stream) => {
+    console.log("New User Joined with ID: ", userID)
+    const video = document.createElement("video")
+    const call = peer.call(userID, stream)
+    call.on("stream", (remoteStream) => {
+        console.log("shivay", remoteStream)
+        addVideoStream(video, remoteStream)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream
+    addVideoStream(myVideo, myVideoStream)
+
+    peer.on("call", call => {
+        call.answer(myVideoStream);
+        const video = document.createElement("video")
+        call.on("stream", (remoteStream) => {
+            console.log("bholenath", remoteStream)
+            addVideoStream(video, remoteStream)
+        }, (error) => {
+            console.log(error)
+        })
+    })
+
+    socket.on("user-connected", (userID) => {
+        console.log("socket.on-user-connected")
+        connectToNewUser(userID, stream)
+    })
+
+}).catch(err => console.log(err))
+
+async function startCamera() {
+
+}
+
+
+async function startScreenShare() {
+    myDisplayStream = await navigator.mediaDevices.getDisplayMedia({
+        video: {
+            cursor: 'always' | 'motion' | 'never',
+            displaySurface: 'application' | 'browser' | 'monitor' | 'window'
+        }
+    })
+    addVideoStream(myVideo, myDisplayStream)
+}
+
+
+const addVideoStream = (video, stream) => {
+    console.log("addvideo stream func")
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+    })
+    videoGrid.append(video)
+}
+
+// peer "open" happens when establishing a connection to my peerJS server is complete.
+peer.on("open", id => {
+    console.log("My user ID is:", id)
+    socket.emit("join-room", roomID, id)
+})
+
+
+
+
+document.getElementById("camera").addEventListener("click", startCamera)
+document.getElementById("screen").addEventListener("click", startScreenShare)
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app)
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const io = require("socket.io")(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    // path: '/myapp',
+    debug: true
+})
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/peerjs", peerServer)
 
 
 app.get("/", (req, res) => {
-    res.render("room")
+    res.redirect(`/${uuidv4()}`)
+})
+
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomID: req.params.room })
+})
+
+
+io.on("connection", (socket) => {
+    console.log("new user joined")
+    socket.on("join-room", (roomID, userID) => {
+        socket.join(roomID);
+        console.log(`A user joind the room ${roomID}`)
+        socket.broadcast.to(roomID).emit("user-connected", userID)
+    })
 })
 
 server.listen(3030, () => {
